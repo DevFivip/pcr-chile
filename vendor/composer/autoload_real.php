e2d103e1bce3235b10aebd0d2565397d115a1f27
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2022-02-10 15:21:34 -0500

    fix

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -65,11 +65,16 @@ class ComposerAutoloaderInit61d168f94de9f1692b28354934e91560
     }
 }
 
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
 function composerRequire61d168f94de9f1692b28354934e91560($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
         $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
